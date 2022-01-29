ba5c3713a5cebcb0d03ff1ebdeac03db6c5d1d9d
Rebecca Ariss <rebeccamarionruth@gmail.com>
2022-01-29 11:16:11 -0500

    Add form validation

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (event) => (event.target === modal ? modal.classList.remove('show-modal') : false));
 
+// Validate form:
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // Valid:
+    return true;
+}
+
 // Handle data from form:
 function storeBookmark(event) {
     // preventDefault method ensures that while this is a static webpage (no back-end component), the default behaviour for an HTML form (sending request to server) is *not* triggered:
@@ -27,6 +43,9 @@ function storeBookmark(event) {
         urlValue = `https://${urlValue}`;
     }
     console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
 }
 
 // Event listener:
